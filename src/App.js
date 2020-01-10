9648c817278661a6c2ca014c8c68c812447a34b0
amlane <twelvexstring@gmail.com>
2020-01-09 21:21:03 -0600

    completed date picker stretch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  //get today's date to set to initial photoDate state
+  let today = new Date();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
+  today = yyyy + "-" + mm + "-" + dd;
+
+  const [photoOfTheDay, setPhotoOfTheDay] = useState({});
+  const [errMsg, setErrMsg] = useState("");
+  const [photoDate, setPhotoDate] = useState(today);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&date=${photoDate}`
+      )
+      .then(res => {
+        // console.log(res);
+        setPhotoOfTheDay(res.data);
+      })
+      .catch(err => {
+        // console.log(err);
+        setErrMsg(`${err.response.status} ${err.response.statusText}`);
+      });
+  }, [photoDate, errMsg]);
+
+  const handleChange = event => {
+    console.log(event.target.value);
+    setPhotoDate(event.target.value);
+  };
+
+  let { date, explanation, title, url } = photoOfTheDay;
+  console.log(photoDate);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <input type="date" onChange={handleChange} />
+      <p>{date}</p>
+      <p>{title}</p>
+      <img src={url} alt={title} />
+      <p>{explanation}</p>
     </div>
   );
 }
